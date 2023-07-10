3d539de04b26d7e46bc7b2a12c4159d0823ada6f
Vandrak9 <haluza@onet.sk>
2023-07-10 15:36:08 +0200

    c

--- progres.php
+++ progres.php
@@ -2,31 +2,47 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Animácia loading po odoslaní formulára</title>
+    <title>Animácia načítavania stránky</title>
     <style>
-        #loading {
-            display: none;
-            text-align: center;
-            margin-top: 20px;
+        #loading-container {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #f1f1f1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 9999;
+        }
+
+        .loader {
+            border: 16px solid #3498db;
+            border-radius: 50%;
+            border-top: 16px solid #ffffff;
+            width: 120px;
+            height: 120px;
+            animation: spin 2s linear infinite;
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <h2>Animácia loading po odoslaní formulára</h2>
-        <form id="myForm">
-            <div class="form-group">
-                <label for="macAddress">MAC adresa:</label>
-                <input type="text" name="macAddress" id="macAddress">
-            </div>
-            <button type="submit">Odoslať</button>
-        </form>
-        <div id="loading">
-            <img src="loading.gif" alt="Loading">
-            <p>Čakajte prosím...</p>
-        </div>
+    <div id="loading-container">
+        <div class="loader"></div>
     </div>
 
-    <script src="script.js"></script>
+    <script>
+        // Zavolajte túto funkciu, keď je stránka plne načítaná
+        window.addEventListener('load', function() {
+            var loadingContainer = document.getElementById('loading-container');
+            loadingContainer.style.display = 'none';
+        });
+    </script>
 </body>
 </html>
