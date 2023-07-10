3b8f57caf143c9358c422daf767ac3b7325a78fc
Vandrak9 <haluza@onet.sk>
2023-07-10 13:46:56 +0200

    commit

--- menu_change_visibility.js
+++ menu_change_visibility.js
@@ -5,14 +5,15 @@ var input2 = document.getElementById('ONU_BRIDGE');
 select.addEventListener('change', handleSelectChange);
 
 function handleSelectChange() {
+
+    input1.classList.add('hidden');
+    input2.classList.add('hidden');
+
     if (select.value === 'ROUTER') {
-        input1.style.display = 'block';
-        input2.style.display = 'none';
-    } else if (select.value === 'BRIDGE') {
-        input1.style.display = 'none';
-        input2.style.display = 'block';
-    } else {
-        input1.style.display = 'block';
-        input2.style.display = 'block';
-    }
+         input1.classList.remove('hidden');
+    } else (select.value === 'BRIDGE') {
+        input2.classList.remove('hidden');
+    } 
 }
+
+
