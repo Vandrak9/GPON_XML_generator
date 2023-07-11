f52c003ea888380fdebc1a3ccf29eb04de20b344
Vandrak9 <haluza@onet.sk>
2023-07-11 12:44:17 +0200

    ADd post request var

--- progress.php
+++ progress.php
@@ -2,11 +2,28 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $numberofcontract = $_POST['numberofcontract'];
+    $serialONU = $_POST['serialONU'];
+    $macAddress = $_POST['macAddress'];
+    $OLT = $_POST['OLT'];
+    $ONU_mode = $_POST['ONU_mode'];
+    $ONU_ROUTER = $_POST['ONU_ROUTER'];
+    $ONU_BRIDGE = $_POST['ONU_BRIDGE'];
+    
+
+
+
+
 
     // Tu môžete spracovať hodnoty premenných $name a $email
 
     // Príklad výstupu:
     echo 'Meno: ' . $name . '<br>';
-    echo 'E-mail: ' . $numberofcontract;
+    echo 'Číslo zmluvy: ' . $numberofcontract;
+    echo 'Sériové číslo ONU: ' . $serialONU . '<br>';
+    echo 'MAC adresa: ' . $macAddress . '<br>';
+    echo 'OLT: ' . $OLT . '<br>';
+    echo 'Mód ONU:' . $ONU_mode . '<br>';
+    echo 'Model ONU v móde ROUTER: ' . $ONU_ROUTER . '<br>';
+    echo 'Model ONU v móde BRIDGE: ' . $ONU_BRIDGE . '<br>';
 }
 ?>
