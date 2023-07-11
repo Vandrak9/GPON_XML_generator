bfc9546a1b02ca8289b8b3ed23f45d1d95de89f9
Vandrak9 <haluza@onet.sk>
2023-07-11 12:24:39 +0200

    commit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div class="container">
         <h2>GPON XML Generator</h2>
         
-        <form action="progress.php" method="POST">
+        <form action=progress.php method=POST>
             <div class="form-group">
                 <label for="name">Meno a priezvisko:</label>
                 <input type="text" name="name" id="name" required>
